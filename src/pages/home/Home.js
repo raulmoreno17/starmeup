3084eff87a0fe1f5b88fa3251faec6d539c0a405
Raul Moreno <raul.mor.1708@gmail.com>
2022-05-24 11:30:11 -0500

    Feat: Add height to main feed

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -7,14 +7,14 @@ const Home = () => {
   return (
     <Grid container spacing={2} className="home__container">
       <Grid item xs={12}>
-      <ValuesPanel/>
+        <ValuesPanel />
       </Grid>
       <Grid item xs={4}>
         <h3>Profile Board</h3>
         <h3>Trending Panel</h3>
       </Grid>
       <Grid item xs={4}>
-        <h3>Main feed</h3>
+        <h3 style={{ height: "2000px" }}>Main feed</h3>
       </Grid>
       <Grid item xs={4}>
         <h3>Surveys</h3>
