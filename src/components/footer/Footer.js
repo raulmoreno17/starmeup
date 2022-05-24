93be66daf3f6a6049c14970f20fdb95ac6288efa
Raul Moreno <raul.mor.1708@gmail.com>
2022-05-24 10:04:12 -0500

    Feat: Footer bar upgraded

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,9 +1,26 @@
 import React from "react";
+import "./Footer.scss";
 
 function Footer() {
   return (
-    <div>
-      <p>This is a footer</p>
+    <div className="footer">
+      <div className="footer__items-container">
+        <span className="footer__item">About StarMeUp ▼</span>
+        <div className="footer__divider"></div>
+        <span className="footer__item">Quick Tour</span>
+        <div className="footer__divider"></div>
+        <span className="footer__item">Language ▼</span>
+        <div className="footer__divider"></div>
+        <span className="footer__item">Cookies</span>
+        <div className="footer__divider--dot">·</div>
+        <span className="footer__item">
+          Developed by <strong>Globant</strong>
+        </span>
+        
+      </div>
+      <div className="footer__brand">
+          <img src={require("../../assets/globant.png")} alt="SVG"></img>
+        </div>
     </div>
   );
 }
